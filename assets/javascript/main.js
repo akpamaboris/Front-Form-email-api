f66c7e3533d9ba3a953188e00fdf9f0bd6a19716
Zeliwipin <zeliwipin@gmail.com>
2021-04-23 23:08:54 +0200

    first commit

---
+++ assets/javascript/main.js
@@ -0,0 +1,20 @@
+const $ = document;
+
+$.addEventListener("DOMContentLoaded", () => {
+  console.log("the document has been loaded");
+
+  $.querySelector(".t-f").addEventListener("submit", async (event) => {
+    event.preventDefault();
+    console.log("on submit activated");
+    const data = {
+      firstName: $.querySelector("#firstname").value,
+      lastName: $.querySelector("#lastname").value,
+      email: $.querySelector("#email").value,
+      message: $.querySelector("#message").value,
+    };
+    console.log(data);
+
+    const response = await axios.post("http://localhost:3000/post", data);
+    console.log(response);
+  });
+});
